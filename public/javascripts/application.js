ea851e5fd11b7fe7c336222c4e8e7779267c2b7d
bentbrewer <jaraddowning@gmail.com>
2012-03-13 16:23:56 -0400

    satisfied

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,7 +5,7 @@ Event.addBehavior({
 
     "form.project select.project-standard:change": function(ev) {
         Hobo.ajaxRequest(window.location.href, ['area-menu'], 
-                         {params: Form.serializeElements([this]), method: 'get', spinnerNextTo: this, message: "Please wait"} )
+                         {params: Form.serializeElements([this]), method: 'get', message: "Filtering list"} )
     }
     
 })
