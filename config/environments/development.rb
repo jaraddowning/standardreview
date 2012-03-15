1a0862c6dba191b691f1d1c0135ef42b7ea1b47f
Jarad Downing <emap@csg.org>
2012-03-15 15:49:25 -0400

    link to std download

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,21 +23,5 @@ Standardreview::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  # Email configuration for CSG Exchange Servers (requires ruby-ntlm gem)
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default :content_type => "text/html"
-  config.action_mailer.smtp_settings = {
-    :address              => "mail.csg.org",
-    :port                 => 25,
-    :domain               => "csg.org",
-    :authentication       => :ntlm,
-    :user_name            => 'emap@csg.org',
-    :password             => 'Emap8222',
-    :enable_starttls_auto => false
-  }
-
 end
 
